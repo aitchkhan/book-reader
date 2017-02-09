8b10ed5666820c583bcfbbec7298fdcc15b89e38
Haroon Khan <aitchkhan@gmail.com>
2017-02-10 01:28:12 +0500

    removed unwanted stuff

--- config.js
+++ config.js
@@ -1,7 +1,3 @@
-var mongoose = require('mongoose');
-
-//mongoose.connect('mongodb://localhost/test');
-
 let pathToBooks = '/var/www/html/buybye/'
 
 module.exports = { 
--- routes.js
+++ routes.js
@@ -1,6 +1,5 @@
 /*End Point Manages here*/
 
 module.exports = function(app) {
-	app.use('/api/v1/user', require('./app/clients/api/v1/user/'));
 	app.use('/api/v1/files', require('./app/clients/api/v1/files/'));
 }
